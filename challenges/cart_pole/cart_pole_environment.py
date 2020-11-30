6cf9aed787b159ccd4235e94f96c9ede2d93febb
Victor Trejo <vic3jo@gmail.com>
2020-11-29 22:02:01 -0400

    Add environment abstract class

--- challenges/cart_pole/cart_pole_environment.py
+++ challenges/cart_pole/cart_pole_environment.py
@@ -1,25 +1,50 @@
 import numpy as np
 import gym
 from common.helpers import make_discrete
+from common.environment import Environment
 
-class CartPoleEnvironment:
+class CartPoleEnvironment(Environment):
   intervals = 9
   env_name = 'CartPole-v1'
   
   def __init__(self):
+    super().__init__()
     self.__environment = gym.make(CartPoleEnvironment.env_name).env
     self.__environment.seed(0)
 
   def get_initial_state(self):
+    """Gets the initial state of an environment
+
+    Returns:
+        tuple: A tuple of of numbers representing the state
+    """
     return self.get_state(self.get_initial_observation())
 
   def get_initial_observation(self):
+    """Gets the initial observation of an environment
+
+    Returns:
+        object: Initial observation
+    """
     return self.__environment.reset()
 
   def apply_action(self, action):
+    """Applies an action to an environment
+
+    Args:
+        action (integer): Id of action to apply
+    """
     return self.__environment.step(action)
 
   def get_state(self, observation):
+    """Maps an observation to state
+
+    Args:
+        observation (object): Observation from the environment
+
+    Returns:
+        tuple: A tuple of of numbers representing the state
+    """
     values = make_discrete(
       observation,
       self.__environment.observation_space,
@@ -46,6 +71,11 @@ class CartPoleEnvironment:
     )
 
   def get_states_dimension(self):
+    """Gets the dimension of the state
+
+    Returns:
+        tuple: A tuple of integer that contains the number of values for dimension
+    """
     return (
       # Cart position
       CartPoleEnvironment.intervals,
@@ -58,7 +88,14 @@ class CartPoleEnvironment:
     )
 
   def get_number_of_actions(self):
+    """Gets the number of actions that can be applied to the environment
+
+    Returns:
+        integer: Number of actions that can be applied
+    """
     return self.__environment.action_space.n
 
   def render(self):
+    """Renders the environment
+    """
     self.__environment.render()
--- common/environment.py
+++ common/environment.py
@@ -1,16 +1,6 @@
-import numpy as np
-import gym
-from common.helpers import make_discrete
-from common.environment import Environment
-
-class MountainCarEnvironment(Environment):
-  intervals = 40
-  env_name = 'MountainCar-v0'
-  
-  def __init__(self):
-    super().__init__()
-    self.__environment = gym.make(MountainCarEnvironment.env_name).env
-    self.__environment.seed(0)
+class Environment:
+  """An abstraction of the environment that an agent interacts with.
+  """  
 
   def get_initial_state(self):
     """Gets the initial state of an environment
@@ -18,7 +8,7 @@ class MountainCarEnvironment(Environment):
     Returns:
         tuple: A tuple of of numbers representing the state
     """
-    return self.get_state(self.get_initial_observation())
+    raise Exception('Method not implemented')
 
   def get_initial_observation(self):
     """Gets the initial observation of an environment
@@ -26,7 +16,7 @@ class MountainCarEnvironment(Environment):
     Returns:
         object: Initial observation
     """
-    return self.__environment.reset()
+    raise Exception('Method not implemented')
 
   def apply_action(self, action):
     """Applies an action to an environment
@@ -34,7 +24,7 @@ class MountainCarEnvironment(Environment):
     Args:
         action (integer): Id of action to apply
     """
-    return self.__environment.step(action)
+    raise Exception('Method not implemented')
 
   def get_state(self, observation):
     """Maps an observation to state
@@ -45,32 +35,25 @@ class MountainCarEnvironment(Environment):
     Returns:
         tuple: A tuple of of numbers representing the state
     """
-    return make_discrete(
-      observation,
-      self.__environment.observation_space,
-      MountainCarEnvironment.intervals,
-    )
+    raise Exception('Method not implemented')
 
   def get_states_dimension(self):
     """Gets the dimension of the state
 
     Returns:
         tuple: A tuple of integer that contains the number of values for dimension
-    """
-    return (
-      MountainCarEnvironment.intervals,
-      MountainCarEnvironment.intervals,
-    )
+    """    
+    raise Exception('Method not implemented')
 
   def get_number_of_actions(self):
     """Gets the number of actions that can be applied to the environment
 
     Returns:
         integer: Number of actions that can be applied
-    """ 
-    return self.__environment.action_space.n
+    """    
+    raise Exception('Method not implemented')  
 
   def render(self):
     """Renders the environment
     """
-    self.__environment.render()
+    raise Exception('Method not implemented')
